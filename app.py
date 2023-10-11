1420f070b157e23eaa2ff73474f50f4c6bbaa6b4
PLAY <145706770+PLAY-A@users.noreply.github.com>
2023-10-11 12:41:41 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+
+# from flask import Flask,request, url_for, redirect, render_template
+# import pickle
+# import numpy as np
+
+# app = Flask(__name__)
+
+# # model=pickle.load(open('model.pkl','rb')) your respective Trained ML model 
+
+
+# @app.route('/')
+# def hello_world():
+#     return render_template("home.html")
+
+
+# # @app.route('/predict',methods=['POST','GET'])
+# # def predict():
+# #     int_features=[int(x) for x in request.form.values()]
+# #     final=[np.array(int_features)]
+# #     print(int_features)
+# #     print(final)
+# #     # prediction=model.predict_proba(final)
+# #     # output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
